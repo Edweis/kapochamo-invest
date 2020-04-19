2b6d291e3cb4500eb1102e70af6e9bc685ef904f
Francois Rulliere <francoisrulliere@SG-CEM-028.local>
2020-04-19 12:41:30 +0800

    Add test for klines

--- src/api.test.js
+++ src/api.test.js
@@ -34,4 +34,31 @@ describe('should test on public endpoints', () => {
     expect(result.status).toEqual(200);
     expect(result.data.bids.length).toEqual(100);
   });
+  it('/klines', async () => {
+    const params = {
+      symbol: SYMBOL,
+      limit: 1,
+      interval: '1m',
+      startTime: 1587271070010,
+    };
+    const expected = [
+      1587271080000, // Open time
+      '0.02582800', // Open
+      '0.02583300', // High
+      '0.02582400', // Low
+      '0.02583200', // Close
+      '99.65300000', // Volume
+      1587271139999, // Close time
+      '2.57390898', // Quote asset volume
+      21, // Number of trades
+      '16.91700000', // Taker buy base asset volume
+      '0.43693429', // Taker buy quote asset volume
+      '0', // Ignore.
+    ];
+    const result = await binancePublic
+      .get('/klines', { params })
+      .catch(handleError);
+    expect(result.status).toEqual(200);
+    expect(result.data).toEqual([expected]);
+  });
 });
