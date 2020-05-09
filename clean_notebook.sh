742f29a5f7e74514d2e9329c432eef75126eece1
Francois Rulliere <francoisrulliere@SG-CEM-028.local>
2020-05-09 11:30:19 +0800

    Add test websocket

--- clean_notebook.sh
+++ clean_notebook.sh
@@ -3,7 +3,7 @@ echo '✨  Pretty notebooks';
 for file in ./src/charts/*.ipynb; do
     [ -f "$file" ] || break
     output=${file/\.ipynb/\.py}
-    echo " - writing in $output"
+    echo "   ✔ writing in $output"
     jq -j '.cells
           | map( select(.cell_type == "code") | .source + ["\n\n"] )
           | .[][]' \
---
+++ src/functions/websocket.ts
@@ -0,0 +1,18 @@
+import WebSocket from 'ws';
+import moment from 'moment';
+const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@aggTrade');
+
+let now: number;
+ws.on('open', () => {
+  now = moment().unix() * 1000;
+  console.log('connected', now);
+});
+
+ws.on('close', () => console.log('disconnected'));
+
+ws.on('message', (message: string) => {
+  const data = JSON.parse(message);
+  console.log((Number(data.E) - now) / 60000);
+});
+
+ws.on('error', console.error);
