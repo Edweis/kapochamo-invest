128d258fcfb70138b136b1b5a10667f3dbb861f2
François Rullière <kapochamo@gmail.com>
2020-05-18 22:46:48 +0800

    Add AddedAt in database

--- src/functions/trader/handler.ts
+++ src/functions/trader/handler.ts
@@ -1,17 +1,17 @@
 import HttpStatus from 'http-status-codes';
-import { successResponse, runWarm, errorResponse } from '../../helpers';
+import { successResponse, runWarm } from '../../helpers';
 import { simulateBuyNow } from './simulation';
 import { sendEmail } from '../../services/aws/sns';
 import { parseMessage } from './parse';
 
-const testfunc: Function = async (event: AWSLambda.SQSEvent) => {
+const traderLambda: Function = async (event: AWSLambda.SQSEvent) => {
   const message = parseMessage(event);
   await sendEmail(`Started with ${JSON.stringify(message)}`);
 
-  const report = await simulateBuyNow('BTCUSDT').catch(errorResponse);
+  const report = await simulateBuyNow('BTCUSDT');
 
   await sendEmail(JSON.stringify(report, null, '\t'));
   return successResponse({ message: 'Success', report, event }, HttpStatus.OK);
 };
 
-export default runWarm(testfunc);
+export default runWarm(traderLambda);
--- src/functions/watcher/handler.ts
+++ src/functions/watcher/handler.ts
@@ -17,7 +17,7 @@ const testfunc: Function = async (event: {}) => {
   const info = await scrapPageInfo(browser, url);
 
   await sendToTrader({ symbol: 'BTCUSDT', info });
-  await updateNews(info);
+  await updateNews({ ...info, addedAt: new Date().toString() });
 
   return successResponse({ message: 'Success', event }, HttpStatus.OK);
 };
