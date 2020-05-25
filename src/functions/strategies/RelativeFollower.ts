886575f0ef2378f484de88c095e8170cc3454703
François Rullière <kapochamo@gmail.com>
2020-05-25 18:49:57 +0800

    typo

--- src/functions/strategies/RelativeFollower.ts
+++ src/functions/strategies/RelativeFollower.ts
@@ -7,7 +7,7 @@ type Percentage = number;
 class RelativeFollower extends Strategy {
   private sellAfterRelativeLossOf: Percentage;
 
-  private pureLossApetite: Percentage;
+  private pureLossAppetite: Percentage;
 
   private boughtFor: number;
 
@@ -15,16 +15,16 @@ class RelativeFollower extends Strategy {
 
   constructor(
     sellAfterRelativeLossOf: Percentage,
-    pureLossApetite: Percentage = 0.2
+    pureLossAppetite: Percentage = 0.2
   ) {
     super(
-      `relativeFollower_L${pureLossApetite * 100}_S${sellAfterRelativeLossOf *
+      `relativeFollower_L${pureLossAppetite * 100}_S${sellAfterRelativeLossOf *
         100}`
     );
     checkPercentage(sellAfterRelativeLossOf);
-    checkPercentage(pureLossApetite);
+    checkPercentage(pureLossAppetite);
     this.sellAfterRelativeLossOf = sellAfterRelativeLossOf;
-    this.pureLossApetite = pureLossApetite;
+    this.pureLossAppetite = pureLossAppetite;
   }
 
   init = (tick: Tick) => {
@@ -37,7 +37,7 @@ class RelativeFollower extends Strategy {
     const gain = Math.max((tick.close - this.boughtFor) / this.boughtFor, 0);
     const triggerCoef =
       gain === 0
-        ? 1 - this.pureLossApetite
+        ? 1 - this.pureLossAppetite
         : 1 - gain * this.sellAfterRelativeLossOf;
     return tick.close <= this.highest * triggerCoef;
   };
