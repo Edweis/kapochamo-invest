aad405f51a5f644f8c0251aaeee2469149c79273
François Rullière <kapochamo@gmail.com>
2020-06-02 20:28:22 +0800

    functional chart that diaply at a specific time

--- view-candle/handler.py
+++ view-candle/handler.py
@@ -1,19 +1,61 @@
 import json
-import numpy as np
 from jinja2 import Template
+from datetime import datetime
+import requests
+
+base_url = 'https://api.binance.com/api/v3'
+parameters = {
+    'symbol': 'BTCUSDT',
+    'limit': 1000,
+    'interval': '1m'
+}
+columns = [
+    'open_time',
+    'open',
+    'high',
+    'low',
+    'close',
+    'volume',
+    'close_time',
+    'quote_asset_volume',
+    'number_of_trades',
+    'taker_buy_base_asset_volume',
+    'taker_buy_quote_asset_volume',
+    'ignore',
+]
+
 
 def main(event, context):
-    a = np.arange(15).reshape(3, 5)
+    print(event)
+    time = 1587271070010
+    if type(event) != 'str':
+        if 'queryStringParameters' in event:
+            qs  = event['queryStringParameters']
+            print(qs)
+            if qs is not None:
+                if 'time' in qs:
+                    time = qs['time']
+    parameters['startTime'] = time
+
+    # Get ticks
+    tick_response = requests.get(base_url+'/klines', parameters)
+    ticks = tick_response.json()
+    formated_ticks = [
+        {"x": tick[0], "y": [float(v) for v in tick[1:5] ]} for tick in ticks
+    ]
+
+    # fill template
     template = Template(open('template.html').read())
-    body = template.render(name='François')
+    body = template.render(name='François', data=formated_ticks)
+
 
     response = {
         "statusCode": 200,
         "body": body,
         "headers": { 'Content-Type': 'text/html' }
     }
-
     return response
+
 
 if __name__ == "__main__":
     print(main('', ''))
