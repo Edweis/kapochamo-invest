e714d8f4e59ff6c505685aee8b01326dde84672e
François Rullière <kapochamo@gmail.com>
2020-06-10 00:05:00 +0800

    remove negative time

--- src/functions/watcher/profiling.ts
+++ src/functions/watcher/profiling.ts
@@ -3,11 +3,29 @@ type ProfilingReport = Array<{ name: string; hrtime: HRTime }>;
 
 const displayTimeDiff = (name: string, start: HRTime, end: HRTime) => {
   const diff = end[1] - start[1];
-  const nano = diff % 1000;
-  const micro = ((diff - nano) / 1000) % 1000;
-  const milli = (diff - micro * 1000 - nano) / 1000000;
-  const sec = end[0] - start[0];
-  return `${name.padEnd(20)}: ${sec}s ${milli}ms ${micro}μs ${nano}ns`;
+  let nano = diff % 1000;
+  let micro = ((diff - nano) / 1000) % 1000;
+  let milli = (diff - micro * 1000 - nano) / 1000000;
+  let sec = end[0] - start[0];
+
+  if (nano < 0) {
+    micro -= 1;
+    nano = 1000 + nano;
+  }
+  if (micro < 0) {
+    milli -= 1;
+    micro = 1000 + micro;
+  }
+  if (milli < 0) {
+    sec -= 1;
+    milli = 1000 + milli;
+  }
+  let result = `${name.padEnd(20)}: `;
+  result += `${sec.toString().padStart(3)}s `;
+  result += `${milli.toString().padStart(3)}ms `;
+  result += `${micro.toString().padStart(3)}μs `;
+  result += `${nano.toString().padStart(3)}ns`;
+  return result;
 };
 class Profiling {
   start: HRTime;
